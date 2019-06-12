96a83758ceb990b4f708211a3bf88d94797f7703
Noah124 <38866103+Noah124@users.noreply.github.com>
2019-06-11 22:06:01 -0400

    Update FreeCodeCampProject(CaesarsCipher).js

--- FreeCodeCampProject(CaesarsCipher).js
+++ FreeCodeCampProject(CaesarsCipher).js
@@ -1,1 +1,26 @@
+function rot13(str) { // LBH QVQ VG!
+  
+  var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'];
+  str = str.split("");
 
+  for(var i = 0; i <str.length; i++){
+    for(var j = 0; j < 25; j++) {
+      if(str[i] == alphabet[j]) {
+        str[i] = alphabet[j+13];
+        console.log(str)
+      }
+
+    }
+  }
+
+  //console.log(str);
+
+  str = str.join("");
+
+  //console.log(str);
+
+  return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
